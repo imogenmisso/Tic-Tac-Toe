24a6f8d545fc321cfeba3ff0a3ad450c19908e0b
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-05 09:41:38 +0000

    Interface send input to board

--- src/interface.js
+++ src/interface.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
   var game = new Game();
 
   document.addEventListener("click", function(event) {
-    console.log(event.target.id)
     if (game.currentPlayer() === "Player 1") {
       document.getElementById(event.target.id).innerHTML = "X"
+      game.makeMove("X", event.target.id)
     } else {
       document.getElementById(event.target.id).innerHTML = "O"
+      game.makeMove("O", event.target.id)
     }
     game.switchPlayer();
   })
