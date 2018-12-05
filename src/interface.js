e82f8e85352989b96ca38fa3ad81eaaee62361b3
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-05 12:23:22 +0000

    Brings up 'play again' button after win

--- src/interface.js
+++ src/interface.js
@@ -8,14 +8,25 @@ $(document).ready(function() {
         game.makeMove("X", event.target.id)
         if (game.win() === "Player 1 wins!") {
           $("#winner").text("Player 1 wins!")
+          showButton()
         }
       } else {
         document.getElementById(event.target.id).innerHTML = "O"
         game.makeMove("O", event.target.id)
         if (game.win() === "Player 2 wins!") {
           $("#winner").text("Player 2 wins!")
+          showButton()
         }
       }
       game.switchPlayer();
     }})
+
+  $("#restart").click(function() {
+    game.resetGame();
+  })
+
+  function showButton() {
+    $("#restart").show();
+  }
+
 })
