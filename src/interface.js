b82cf77bd892e9069e6551f48c61955cb32b1e98
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-05 11:36:15 +0000

    Updated interface so user cannot click same square twice

--- src/interface.js
+++ src/interface.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
   var game = new Game();
 
-  document.addEventListener("click", function(event) {
-    if (game.currentPlayer() === "Player 1") {
-      document.getElementById(event.target.id).innerHTML = "X"
-      game.makeMove("X", event.target.id)
-      if (game.win() === "Player 1 wins!") {
-        $("#winner").text("Player 1 wins!")
+  $("#board").click(function(event) {
+    if (game.board[event.target.id] === "") {
+      if (game.currentPlayer() === "Player 1") {
+        document.getElementById(event.target.id).innerHTML = "X"
+        game.makeMove("X", event.target.id)
+        if (game.win() === "Player 1 wins!") {
+          $("#winner").text("Player 1 wins!")
+        }
+      } else {
+        document.getElementById(event.target.id).innerHTML = "O"
+        game.makeMove("O", event.target.id)
+        if (game.win() === "Player 2 wins!") {
+          $("#winner").text("Player 2 wins!")
+        }
       }
-    } else {
-      document.getElementById(event.target.id).innerHTML = "O"
-      game.makeMove("O", event.target.id)
-      if (game.win() === "Player 2 wins!") {
-        $("#winner").text("Player 2 wins!")
-      }
-    }
-    game.switchPlayer();
-  })
+      game.switchPlayer();
+    }})
 })
