68879b501e93e9b1279cb611d2305a572d5d6cad
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-12-05 10:03:28 +0000

    Updated interface to display winner

--- src/interface.js
+++ src/interface.js
@@ -5,9 +5,15 @@ $(document).ready(function() {
     if (game.currentPlayer() === "Player 1") {
       document.getElementById(event.target.id).innerHTML = "X"
       game.makeMove("X", event.target.id)
+      if (game.win() === "Player 1 wins!") {
+        $("#winner").text("Player 1 wins!")
+      }
     } else {
       document.getElementById(event.target.id).innerHTML = "O"
       game.makeMove("O", event.target.id)
+      if (game.win() === "Player 2 wins!") {
+        $("#winner").text("Player 2 wins!")
+      }
     }
     game.switchPlayer();
   })
